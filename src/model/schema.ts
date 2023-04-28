061e53a4e9999e0031fc0c97f70f82632dd36639
João Nascimento <joaombn97@gmail.com>
2023-04-27 22:56:12 -0300

    Added interfaces to schemas.

--- src/model/schema.ts
+++ src/model/schema.ts
@@ -1,37 +1,78 @@
-import { Schema, model } from 'mongoose';
+import { Document, Schema, Types, model } from 'mongoose';
 
-const { ObjectId, Decimal128 } = Schema.Types;
+interface IEntry extends Document {
+  comments: string;
+  target: string;
+  timestamp: Date;
+  type: string;
+  value: number;
+  isExpense: boolean;
+}
 
-const entrySchema = new Schema({
+const entrySchema = new Schema<IEntry>({
   comments: String,
-  target: { type: String, require: true },
-  timestamp: { type: Date, require: true },
-  type: { type: String, require: true },
-  value: { type: Decimal128, require: true },
-  isExpense: { type: Boolean, require: true },
-});
-export const Entry = model('Entry', entrySchema, 'entries');
-
-const debtSchema = new Schema({
+  target: { type: String, required: true },
+  timestamp: { type: Date, required: true },
   type: { type: String, required: true },
-  value: { type: Decimal128, required: true },
+  value: { type: Number, required: true },
+  isExpense: { type: Boolean, required: true },
+});
+
+export const Entry = model<IEntry>('Entry', entrySchema, 'entries');
+
+interface IDebt extends Document {
+  type: string;
+  value: number;
+  dueDate: Date;
+}
+
+const debtSchema = new Schema<IDebt>({
+  type: { type: String, required: true },
+  value: { type: Number, required: true },
   dueDate: { type: Date, required: true },
 });
-export const Debt = model('Debt', debtSchema);
 
-const accountSchema: Schema = new Schema({
+export const Debt = model<IDebt>('Debt', debtSchema);
+
+interface IAccount extends Document {
+  name: string;
+  value: number;
+  isNegative: boolean;
+}
+
+const accountSchema = new Schema<IAccount>({
   name: { type: String, required: true },
-  value: { type: Decimal128, required: true },
+  value: { type: Number, required: true },
   isNegative: { type: Boolean, required: true },
 });
 
-const titleSchema: Schema = new Schema({
+interface ITitle extends Document {
+  name: string;
+  type: string;
+  value: number;
+}
+
+const titleSchema = new Schema<ITitle>({
   name: { type: String, required: true },
   type: { type: String, required: true },
-  value: { type: Decimal128, required: true },
+  value: { type: Number, required: true },
 });
 
-const userSchema = new Schema({
+interface IUser extends Document {
+  googleId: string;
+  name?: string;
+  familyName?: string;
+  givenName?: string;
+  picture?: string;
+  email?: string;
+  createdAt: Date;
+  accounts: Types.DocumentArray<IAccount>;
+  titles: Types.DocumentArray<ITitle>;
+  debts: Types.Array<Types.ObjectId>;
+  entries: Types.Array<Types.ObjectId>;
+}
+
+const userSchema = new Schema<IUser>({
   googleId: { type: String, required: true, unique: true },
   name: String,
   familyName: String,
@@ -41,8 +82,8 @@ const userSchema = new Schema({
   createdAt: { type: Date, default: Date.now() },
   accounts: [accountSchema],
   titles: [titleSchema],
-  debts: [ObjectId],
-  entries: [ObjectId],
+  debts: [{ type: Schema.Types.ObjectId, ref: 'Debt' }],
+  entries: [{ type: Schema.Types.ObjectId, ref: 'Entry' }],
 });
 
-export const User = model('User', userSchema);
+export const User = model<IUser>('User', userSchema);
